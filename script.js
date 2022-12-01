d262f290a46b969de41f43d9a905870198261ab9
Luka Stojic <luka@arthaus.info>
2022-12-01 14:48:53 +0100

    added method to convert to gif

--- script.js
+++ script.js
@@ -3,14 +3,27 @@ const ffmpeg = createFFmpeg({
 	log: true,
 });
 
-const transcode = async ({ target: { files } }) => {
+const vidToGif = async ({ target: { files } }) => {
+	const { name } = files[0];
+	await ffmpeg.load();
+	ffmpeg.FS('writeFile', name, await fetchFile(files[0]));
+	await ffmpeg.run('-i', name, 'output.gif');
+	const data = ffmpeg.FS('readFile', 'output.gif');
+	const image = document.createElement('img');
+	document.querySelector('.innerContentWrapper').appendChild(image)
+	image.src = URL.createObjectURL(new Blob([data.buffer], { type: 'image/gif'}));
+};	
+
+const vidToMp4 = async ({ target: { files } }) => {
 	const { name } = files[0];
 	await ffmpeg.load();
 	ffmpeg.FS('writeFile', name, await fetchFile(files[0]));
 	await ffmpeg.run('-i', name, 'output.mp4');
 	const data = ffmpeg.FS('readFile', 'output.mp4');
-	const video = document.getElementById('player');
+	const video = document.createElement('video');
+	document.querySelector('.innerContentWrapper').appendChild(video)
+	video.controls = true
 	video.src = URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4'}));
 };	
 
-document.getElementById('uploader').addEventListener('change', transcode);
+document.getElementById('uploader').addEventListener('change', vidToMp4);
